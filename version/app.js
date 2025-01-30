398de17e091d70c0a1db39008cb8f40c1703b2a4
meetybhuva1326 <meetupwork24@gmail.com>
2025-01-30 15:11:21 +0530

    Add V2 fun

--- version/app.js
+++ version/app.js
@@ -12,6 +12,10 @@ app.get('/', function (req, res) {
 
 });
 
+app.get('/v2', function (req, res) {
+  res.send('This is version 2 of the app.\n');
+});
+
 app.listen(8080, function () {
   console.log('Server listening on port 8080...');
 });
